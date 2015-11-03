124a81919c9f6eb9865539e63e3cc89eb8eb6951
monsteronfire <monsteronfire@gmail.com>
2015-11-03 23:50:56 +0800

    adds attributes to #user_id element. Checker working. Needs refinement

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,30 +9,24 @@ angular.module("stcheckerApp")
             return input;
         }
     })
-    // .directive('watchInput', function(instagram) {
-    //   //var userid = '630194682';
-    //   return function(scope, elem) {
-
-    //   };
-    // })
     .factory('instagram', ['$http',
-        function($http) {
-          var userid = angular.element("userid").val;
+      function($http) {
+        var userid;
+        return {
+          fetchPopular: function(callback, userid) {
+            var userid = angular.element("input#user_id").val();
 
-            return {
-                fetchPopular: function(callback, userid) {
+            var endPoint = "https://api.instagram.com/v1/users/"+userid+"/follows?access_token=630194682.8d0c4ad.c79f81f6f10d43d8867a5c42c90d0d43&callback=JSON_CALLBACK";
 
-                    var endPoint = "https://api.instagram.com/v1/users/"+userid+"/follows?access_token=630194682.8d0c4ad.c79f81f6f10d43d8867a5c42c90d0d43&callback=JSON_CALLBACK";
-
-                    $http.jsonp(endPoint).success(function(response) {
-                        callback(response.data);
-                        console.log(response.data);
-                    });
-                    console.log(userid + ' erm');
-                    return userid;
-                }
-            }
+            $http.jsonp(endPoint).success(function(response) {
+                callback(response.data);
+                console.log(response.data);
+            });
+            console.log(userid);
+            return userid;
+          }
         }
+      }
     ])
     .controller("stcheckerCtrl", function($scope, $interval, instagram) {
       $scope.followers = [];
