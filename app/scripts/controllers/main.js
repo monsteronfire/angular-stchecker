e3c2d3670ac5deefd42d05de5677a6cfd3dea391
monsteronfire <monsteronfire@gmail.com>
2015-11-03 16:48:06 +0800

    adds code to pull IG followers

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,32 +14,29 @@ angular.module("stcheckerApp")
             return {
                 fetchPopular: function(callback) {
 
-                    var endPoint = "https://api.instagram.com/v1/media/popular?client_id=8d0c4adf568146c7968559a9a7ee4d2f&callback=JSON_CALLBACK";
+                    var endPoint = "https://api.instagram.com/v1/users/630194682/follows?access_token=630194682.8d0c4ad.c79f81f6f10d43d8867a5c42c90d0d43&callback=JSON_CALLBACK";
 
                     $http.jsonp(endPoint).success(function(response) {
                         callback(response.data);
+                        console.log(response.data);
                     });
                 }
             }
         }
     ])
     .controller("Example", function($scope, $interval, instagram) {
-      $scope.pics = [];
+      $scope.followers = [];
       $scope.have = [];
       $scope.orderBy = "-likes.count";
       $scope.getMore = function() {
         instagram.fetchPopular(function(data) {
             for(var i=0; i<data.length; i++) {
               if (typeof $scope.have[data[i].id]==="undefined") {
-                $scope.pics.push(data[i]) ;
+                $scope.followers.push(data[i]) ;
                 $scope.have[data[i].id] = "1";
               }
             }
         });
       };
       $scope.getMore();
-      
-        $scope.tags = [
-            'Bootstrap', 'AngularJS', 'Instagram', 'Factory'
-        ]
     });
