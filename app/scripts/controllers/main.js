12b513e20bf89a833db2b72378cc5d55c521cc50
monsteronfire <monsteronfire@gmail.com>
2015-11-03 15:37:42 +0800

    adds instagram api code

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,45 @@
-'use strict';
+angular.module("stcheckerApp")
+    .filter('fromTo', function() {
+        return function(input, from, total, lessThan) {
+            from = parseInt(from);
+            total = parseInt(total);
+            for (var i = from; i < from + total && i < lessThan; i++) {
+                input.push(i);
+            }
+            return input;
+        }
+    })
+    .factory('instagram', ['$http',
+        function($http) {
+            return {
+                fetchPopular: function(callback) {
 
-/**
- * @ngdoc function
- * @name stcheckerApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the stcheckerApp
- */
-angular.module('stcheckerApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+                    var endPoint = "https://api.instagram.com/v1/media/popular?client_id=8d0c4adf568146c7968559a9a7ee4d2f&callback=JSON_CALLBACK";
+
+                    $http.jsonp(endPoint).success(function(response) {
+                        callback(response.data);
+                    });
+                }
+            }
+        }
+    ])
+    .controller("Example", function($scope, $interval, instagram) {
+      $scope.pics = [];
+      $scope.have = [];
+      $scope.orderBy = "-likes.count";
+      $scope.getMore = function() {
+        instagram.fetchPopular(function(data) {
+            for(var i=0; i<data.length; i++) {
+              if (typeof $scope.have[data[i].id]==="undefined") {
+                $scope.pics.push(data[i]) ;
+                $scope.have[data[i].id] = "1";
+              }
+            }
+        });
+      };
+      $scope.getMore();
+      
+        $scope.tags = [
+            'Bootstrap', 'AngularJS', 'Instagram', 'Factory'
+        ]
+    });
