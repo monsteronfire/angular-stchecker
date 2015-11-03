22211593f11fbb1c9f175cd896078f1eabe463dc
monsteronfire <monsteronfire@gmail.com>
2015-11-03 13:25:47 +0800

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name stcheckerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the stcheckerApp
+ */
+angular.module('stcheckerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
